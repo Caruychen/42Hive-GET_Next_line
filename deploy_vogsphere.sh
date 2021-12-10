5413b066c6b07c84e8350b3cfa3c0e52d5425d6b
Caruy Chen <caruychen@gmail.com>
2021-12-10 11:58:35 +0200

    initialise and setup vogsphere actions

---
+++ deploy_vogsphere.sh
@@ -0,0 +1,17 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    deploy_vogsphere.sh                                :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/12/10 11:48:30 by cchen             #+#    #+#              #
+#    Updated: 2021/12/10 11:52:41 by cchen            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+VOGSPHERE=$(ls | sed -n '/intra-uuid/p')
+
+rm -rf ./${VOGSPHERE}/*
+cp ./$REPO_NAME/src/* ./${VOGSPHERE}
+cp -R ./$REPO_NAME/libft ./${VOGSPHERE}/libft
