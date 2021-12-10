ed80e696b5e8507ec4c06d6d72e4c6e0dc552cf3
Caruy Chen <caruychen@gmail.com>
2021-12-10 15:27:29 +0200

    start get_next_line

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,10 +6,14 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 11:45:43 by cchen             #+#    #+#             */
-/*   Updated: 2021/12/10 11:45:59 by cchen            ###   ########.fr       */
+/*   Updated: 2021/12/10 15:27:14 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 int	get_next_line(const int fd, char **line)
 {
+	int	bytes_read;
+	static char	*s_arr[
 }
