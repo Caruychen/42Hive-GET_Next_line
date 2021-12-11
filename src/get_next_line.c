3f048d51e987060fb0d48c9156fefaf9f4cacb07
Caruychen <caruychen@gmail.com>
2021-12-11 16:55:36 +0200

    latest version

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,38 +6,11 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 11:45:43 by cchen             #+#    #+#             */
-/*   Updated: 2021/12/11 16:41:16 by cchen            ###   ########.fr       */
+/*   Updated: 2021/12/11 16:53:27 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-static int	assign_line(char **s_arr, char **line, int bytes)
-{
-	int		len;
-	char	*temp;
-
-	if (bytes < 0)
-		return (-1);
-	if (bytes == 0 && *s_arr == NULL)
-		return (0);
-	len = 0;
-	while ((*s_arr)[len] != '\0' && (*s_arr)[len] != '\n')
-		++len;
-	if ((*s_arr)[len] == '\n')
-	{
-		*line = ft_strsub(*s_arr, 0, len);
-		temp = ft_strdup(&((*s_arr)[len + 1]));
-		ft_strdel(s_arr);
-		*s_arr = temp;
-		if (**s_arr == '\0')
-			ft_strdel(s_arr);
-		return (1);
-	}
-	*line = ft_strdup(*s_arr);
-	ft_strdel(s_arr);
-	return (1);
-}
 
 static int	read_line(const int fd, char *buff, int *bytes)
 {
@@ -59,6 +32,38 @@ static void	cpy_buff(char **s, char *buff)
 	*s = temp;
 }
 
+static int	push_line(char **s_arr, char **line)
+{
+	int		len;
+	char	*temp;
+
+	len = 0;
+	while ((*s_arr)[len] != '\0' && (*s_arr)[len] != '\n')
+		++len;
+	if ((*s_arr)[len] == '\n')
+	{
+		*line = ft_strsub(*s_arr, 0, len);
+		temp = ft_strdup(&((*s_arr)[len + 1]));
+		ft_strdel(s_arr);
+		*s_arr = temp;
+		if (**s_arr == '\0')
+			ft_strdel(s_arr);
+		return (1);
+	}
+	*line = ft_strdup(*s_arr);
+	ft_strdel(s_arr);
+	return (1);
+}
+
+static int	result(char **s_arr, char **line, int bytes)
+{
+	if (bytes < 0)
+		return (-1);
+	if (bytes == 0 && *s_arr == NULL)
+		return (0);
+	return (push_line(s_arr, line));
+}
+
 int	get_next_line(const int fd, char **line)
 {
 	int			bytes;
@@ -74,5 +79,5 @@ int	get_next_line(const int fd, char **line)
 		if (ft_strchr(buff, '\n'))
 			break ;
 	}
-	return (assign_line(&s_arr[fd], line, bytes));
+	return (result(&s_arr[fd], line, bytes));
 }
