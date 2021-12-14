aceed4a200552f07bf5343b2b410038be2b6d5d5
Caruy Chen <caruychen@gmail.com>
2021-12-14 15:41:22 +0200

    fix deployment

--- deploy_vogsphere.sh
+++ deploy_vogsphere.sh
@@ -6,7 +6,7 @@
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/12/10 11:48:30 by cchen             #+#    #+#              #
-#    Updated: 2021/12/14 15:38:59 by cchen            ###   ########.fr        #
+#    Updated: 2021/12/14 15:41:04 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,4 +16,4 @@ rm -rf ./${VOGSPHERE}/*
 cp ./$REPO_NAME/author ./${VOGSPHERE}
 cp ./$REPO_NAME/src/get_next_line.c ./${VOGSPHERE}
 cp ./$REPO_NAME/src/includes/get_next_line.h ./${VOGSPHERE}
-cp -p ./$REPO_NAME/src/libft ./${VOGSPHERE}/libft
+cp -rp ./$REPO_NAME/src/libft ./${VOGSPHERE}/libft
