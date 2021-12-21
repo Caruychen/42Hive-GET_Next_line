89443d5749e920e4e5734f2e8baa46562fa34860
Caruy Chen <caruychen@gmail.com>
2021-12-21 15:13:02 +0200

    update libft

--- src/libft/libft.h
+++ src/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 13:39:10 by cchen             #+#    #+#             */
-/*   Updated: 2021/12/06 19:25:48 by cchen            ###   ########.fr       */
+/*   Updated: 2021/12/21 11:34:55 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <unistd.h>
 # include <string.h>
 # include <stdlib.h>
+# include "vec.h"
 
 //	PART 1
 void	*ft_memset(void *b, int c, size_t len);
@@ -93,5 +94,6 @@ void	ft_memdelarray(void ***arr);
 void	ft_strdelarray(char ***arr);
 int		ft_isupper(int c);
 int		ft_islower(int c);
+int		ft_imin(const int n1, const int n2);
 
 #endif
