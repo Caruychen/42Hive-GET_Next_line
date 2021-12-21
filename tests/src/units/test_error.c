06d44e7a4ef8ea44ba510debd1288e6c632797e9
Caruy Chen <caruychen@gmail.com>
2021-12-21 14:52:49 +0200

    check error tests

--- tests/src/units/test_error.c
+++ tests/src/units/test_error.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 16:49:21 by cchen             #+#    #+#             */
-/*   Updated: 2021/12/16 16:51:09 by cchen            ###   ########.fr       */
+/*   Updated: 2021/12/21 14:05:19 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	test_error(void)
 	int		res;
 
 	res = get_next_line(42, &line);
+	line = malloc(10);
+	free(line);
 	if (res != -1)
 		return (1);
 	return (0);
